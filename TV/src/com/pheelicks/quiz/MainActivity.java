37d532f2019f00c63961996f2ae192f4539214d4
Felix Palmer <felix.palmer@metaswitch.com>
2012-04-21 12:13:43 -0700

    Display buttons

--- TV/src/com/pheelicks/quiz/MainActivity.java
+++ TV/src/com/pheelicks/quiz/MainActivity.java
@@ -1,11 +1,16 @@
 package com.pheelicks.quiz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
   private TextView mQuestionTextView;
+  private List<Button> mOptionButtons;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,20 @@ public class MainActivity extends Activity {
   private void findUIElements()
   {
     mQuestionTextView = (TextView)findViewById(R.id.question_tv);
+    mOptionButtons = new ArrayList<Button>(4);
+    mOptionButtons.add((Button)findViewById(R.id.option_1));
+    mOptionButtons.add((Button)findViewById(R.id.option_2));
+    mOptionButtons.add((Button)findViewById(R.id.option_3));
+    mOptionButtons.add((Button)findViewById(R.id.option_4));
   }
 
   private void setupUI()
   {
     // TODO, load from xml
     mQuestionTextView.setText("What is your favorite squirrel?");
+    mOptionButtons.get(0).setText("Black");
+    mOptionButtons.get(1).setText("Red");
+    mOptionButtons.get(2).setText("Grey");
+    mOptionButtons.get(3).setText("Silver");
   }
 }
