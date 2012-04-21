4ad6663e4cce4599946aafe8891da7851b1fadb3
Felix Palmer <felix.palmer@metaswitch.com>
2012-04-21 11:58:07 -0700

    Start on quiz layout

--- TV/src/com/pheelicks/quiz/MainActivity.java
+++ TV/src/com/pheelicks/quiz/MainActivity.java
@@ -2,12 +2,28 @@ package com.pheelicks.quiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  private TextView mQuestionTextView;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.quiz);
+
+    findUIElements();
+    setupUI();
+  }
+
+  private void findUIElements()
+  {
+    mQuestionTextView = (TextView)findViewById(R.id.question_tv);
+  }
+
+  private void setupUI()
+  {
+    // TODO, load from xml
+    mQuestionTextView.setText("What is your favorite squirrel?");
+  }
 }
