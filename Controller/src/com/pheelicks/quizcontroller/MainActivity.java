7186a2b372ea9e1c0eb04960378aae7a8b34c97d
kentwilliam <kentwilliam@gmail.com>
2012-04-21 23:47:10 -0700

    Updates to background and stuffs.

--- Controller/src/com/pheelicks/quizcontroller/MainActivity.java
+++ Controller/src/com/pheelicks/quizcontroller/MainActivity.java
@@ -3,6 +3,7 @@ package com.pheelicks.quizcontroller;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -11,6 +12,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        // Set up font
+        /*
+        TextView txt = (TextView) findViewById(R.id.custom_font);  
+        Typeface font = Typeface.createFromAsset(getAssets(), "Chantelli_Antiqua.ttf");  
+        txt.setTypeface(font); */
+        
+        // set 32 bit window (draw correctly transparent images)
+        getWindow().getAttributes().format = android.graphics.PixelFormat.RGBA_8888;
+        
         // Launch tcp test
         Intent intent = new Intent(this, QuizClientActivity.class);
         startActivity(intent);
