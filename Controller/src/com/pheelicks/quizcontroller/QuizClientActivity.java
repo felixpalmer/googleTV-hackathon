67c94baa2a8afd72ecb130c98cd9aac042eec525
Felix Palmer <felix.palmer@metaswitch.com>
2012-04-21 20:21:36 -0700

    Added QuizClientActivity

--- Controller/src/com/pheelicks/quizcontroller/QuizClientActivity.java
+++ Controller/src/com/pheelicks/quizcontroller/QuizClientActivity.java
@@ -4,16 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class QuizClientActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.quizclient);
 
         // Launch tcp test
-        Intent intent = new Intent(this, QuizClientActivity.class);
-        startActivity(intent);
-        finish();
+        //Intent intent = new Intent(this, .class);
+       // startActivity(intent);
     }
 }
