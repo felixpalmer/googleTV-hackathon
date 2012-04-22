47e8c3396b042b44c147a2f1bb8c723374be28ae
Felix Palmer <felix.palmer@metaswitch.com>
2012-04-22 13:25:45 -0700

    Start on saving prefs

--- Controller/src/com/pheelicks/quizcontroller/MainActivity.java
+++ Controller/src/com/pheelicks/quizcontroller/MainActivity.java
@@ -2,6 +2,7 @@ package com.pheelicks.quizcontroller;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -10,13 +11,25 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         // set 32 bit window (draw correctly transparent images)
         getWindow().getAttributes().format = android.graphics.PixelFormat.RGBA_8888;
-        
-        // Launch tcp test
-        
-        Intent intent = new Intent(this, InitialScreenActivity.class);
+
+        SharedPreferences prefs = getSharedPreferences("default", MODE_WORLD_READABLE);
+        String ip = prefs.getString(Prefs.IP, "");
+        int clientId = prefs.getInt(Prefs.CLIENT_ID, -1);
+
+        Intent intent;
+
+        if(ip.equalsIgnoreCase("") || clientId == -1)
+        {
+          intent = new Intent(this, InitialScreenActivity.class);
+        }
+        else
+        {
+          intent = new Intent(this, QuizClientActivity.class);
+        }
+
         startActivity(intent);
         finish();
     }
---
+++ Controller/src/com/pheelicks/quizcontroller/Prefs.java
@@ -0,0 +1,7 @@
+package com.pheelicks.quizcontroller;
+
+public interface Prefs
+{
+  public final static String IP = "ip";
+  public final static String CLIENT_ID = "client_id";
+}
