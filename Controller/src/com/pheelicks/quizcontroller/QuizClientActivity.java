d7940266e3658592f5e65cfa99f3025d7e194a8b
Felix Palmer <felix.palmer@metaswitch.com>
2012-04-21 20:44:47 -0700

    Added connect button

--- Controller/src/com/pheelicks/quizcontroller/QuizClientActivity.java
+++ Controller/src/com/pheelicks/quizcontroller/QuizClientActivity.java
@@ -1,18 +1,20 @@
 package com.pheelicks.quizcontroller;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class QuizClientActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quizclient);
+    }
 
-        // Launch tcp test
-        //Intent intent = new Intent(this, .class);
-       // startActivity(intent);
+    public void connectPressed(View view)
+    {
+
     }
 }
